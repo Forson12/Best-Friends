d026e83c6a49fa32bf29780b0d64047be0458b36
forson12 <gabrielforson.gh@gmail.com>
2023-11-14 20:55:02 +0000

    Kobi: Restructuring done with login and regiter

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # Best-Friends
 A group of best friends coding the new world!!
+
+
+# Sexy Yout King - @KOBI
+# BackEnd criteria to be met for your code to work 
+# You need to install 2 main things to get started
+
+- First thing is to download and install nodejs from this site if you don't have it on your laptop / pc already 
+https://nodejs.org/en/download 
+- Make sure you download according to the specs of your pc / laptop (either Windwos 64 or the one for MAc)
+- Follow the installation process, pretty simple to install
+- After installation, open cmd (command prompt) or VSC (Visual Studio Code) terminal and type this to make sure it is installed on our laptop
+# npm --version
+- if the veriosn of node js shows then, everything worked. 
+- Mind you, the version can show but the node might not work on VSC 
+- else you need to add node to your path environment variables 
+- Contact me to help you out or follow the instructions on this page, https://bobbyhadz.com/blog/npm-is-not-recognized-as-internal-or-external-command#:~:text=The%20error%20%22'npm'%20is,your%20system's%20PATH%20environment%20variable.
+
+# You will then have to download and install mongodb if you want to see the login/register working 
+
+- visit this site https://www.mongodb.com/try/download/community
+- under the community server, click on select package 
+- After downloading, install it. 
+- The installation is straigth forward but make sure the mongo compass box is ticked as that's what we will be using 
+- (leave this part for now) download mogo Shell later https://www.mongodb.com/try/download/shell 
+- 
+
+
+ 
+# Now in the terminal of VSCode, do the following  
+Install 3 extensions for this program to work 
+1. express 
+2. hbs - an extension of express we work with (usually ejs) 
+3. mongoose - to help connect mongodb to nodejs 
+
+Note: install all this in the terminal of your working environment, in this case VSCode 
+
+code for this is below 
+# npm i express 
+# npm i ejs 
+# npm i mongoose  
+# npm install express-session
+npm install express body-parser express-session
+
+
+
+
+
+# The notes below is not part, feel free to read through 
+When your teammates clone the repository to their local machines, they will typically need to install the necessary dependencies and set up the development environment. Here are the general steps they might need to follow:
+
+1. **Node.js and npm:**
+   - Ensure that Node.js and npm (Node Package Manager) are installed on their machines. They can download and install them from the official website: [Node.js](https://nodejs.org/).
+
+2. **Install Dependencies:**
+   - Run `npm install` in the project's root directory. This command will install all the dependencies listed in the `package.json` file.
+
+3. **MongoDB (if applicable):**
+   - If your project uses MongoDB, your teammates need to have MongoDB installed on their machines. They can download it from the official website: [MongoDB](https://www.mongodb.com/try/download/community).
+
+4. **Environment Variables:**
+   - If your project uses environment variables (e.g., for database connection strings, API keys), they should set up their own environment variables based on a template or documentation provided by you. You can use a tool like `dotenv` to manage environment variables during development.
+
+5. **Run the Application:**
+   - Once the dependencies are installed, they can run the application using the command specified in your `package.json` file. Typically, it's something like `npm start` or `npm run start`.
+
+6. **Check Documentation:**
+   - If you have any specific instructions or documentation for your project, make sure your teammates read through it. This might include setup instructions, coding conventions, and any other important information.
+
+7. **Database Setup (if applicable):**
+   - If your project involves a database, they might need to run database migrations or seed data to set up the initial database state. Check if your project has any scripts or commands for this purpose.
+
+8. **Coding Standards and Editor Configurations:**
+   - If you have defined coding standards or editor configurations (e.g., `.editorconfig`), ask your teammates to follow them to maintain consistency in the codebase.
+
+Remember, these steps may vary depending on the specifics of your project, so it's a good idea to provide clear documentation and instructions for your teammates. Additionally, you can include a `README.md` file in your repository with information on how to set up and run the project.
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,307 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session'); // Add express-session middleware
+
+//User model definitions
+const User = mongoose.model('User', new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    orgName: { type: String, default: ''  },
+    phoneNumber: { type: String, default: ''  },
+    logo: { type: String, default: ''  },
+    instaHandles: { type: String, default: ''  },
+    twitterHandles: { type: String, default: ''  },
+    linkHandles: { type: String, default: '' },
+    comments: { type: String, default: ''  },
+}));
+
+
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Generating random string using node js 
+const crypto = require('crypto');
+const secretKey = crypto.randomBytes(32).toString('hex');
+console.log(secretKey);
+
+
+
+// Use express-session middleware
+app.use(session({
+  secret: secretKey, // Replace with a strong secret key
+  resave: true,
+  saveUninitialized: true
+}));
+
+const dbUrl = 'mongodb://localhost:27017/Alturo';
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+
+// Registration route
+app.post('/registration', async (req, res) => {
+  const { username, email, password, confirmPassword } = req.body;
+
+  // Check if password and confirmPassword match
+  if (password !== confirmPassword) {
+    return res.status(400).send('Passwords do not match');
+  }
+
+  try {
+    // Check if the user already exists
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(400).send('Username or email already exists');
+    }
+
+    // Create a new user
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+
+    // Redirect to the login page after successful registration
+    res.redirect('/login'); // Update the path based on your project structure
+  } catch (error) {
+    console.error('Error during registration:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+// // Login route
+// app.post('/login', async (req, res) => {
+//     const { username, password } = req.body;
+
+//     try {
+//         // Check if the user exists in the database
+//         const user = await User.findOne({ username, password });
+//         if (user) {
+//             // Store user information in the session after successful login
+//             req.session.user = {
+//                 _id: user._id, // Make sure to include the user's ID
+//                 username: user.username,
+//                 email: user.email,
+//                 // Add more user data as needed
+//             };
+
+//             // Redirect to the welcome page after successful login
+//             res.redirect('/displayOrganisation'); // Update the path based on your project structure
+//         } else {
+//             // User not found, handle login failure
+//             res.status(401).send('Invalid username or password');
+//         }
+//     } catch (error) {
+//         console.error('Error during login:', error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// });
+
+
+// Login route
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        // Check if the user exists in the database
+        const user = await User.findOne({ username, password });
+
+        if (user) {
+            // Store user and organization information in the session after successful login
+            req.session.user = {
+                _id: user._id, // Make sure to include the user's ID
+                username: user.username,
+                email: user.email,
+                // Add more user data as needed
+            };
+
+            // Fetch organization details from the database based on user ID
+            const organization = await User.findOne({ _id: user._id });
+
+            // Store organization details in the session
+            req.session.organizationDetails = organization;
+
+            // Redirect to the displayOrganisation page after successful login
+            res.redirect('/displayOrganisation');
+        } else {
+            // User not found, handle login failure
+            res.status(401).send('Invalid username or password');
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
+
+// Set view engine to ejs
+// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+/*Route to the index page */
+app.get('/', function (req, res) {
+  console.log('Handling request for /index or landing page');
+  res.render('pages/index');
+});
+
+
+/* Route to the Contact Page*/
+  app.get('/contactUs', function (req, res) {
+    console.log('Handling request for / contactUs');
+    res.render('pages/contact');
+  });
+
+
+/*Route to the Login Page */
+app.get('/login', function (req, res) {
+  console.log('Handling request for /login');
+  res.render('pages/login');
+});
+
+
+// /*Route to the addOrganisation Page */
+// app.get('/addOrganisation', function (req, res) {
+//     console.log('Handling request for /addOrganisation');
+//     res.render('pages/addOrganisation');
+//   });
+
+
+/*Route to the registration page */
+app.get('/registration', function (req, res) {
+  console.log('Handling request for /registration');
+  res.render('pages/registration');
+});
+
+
+
+
+// Route for handling the /addOrganization page
+
+
+app.get('/addOrganisation', (req, res) => {
+    // Assuming you've stored the user in the session upon login
+    const user = req.session.user;
+  
+    // Check if the user is logged in
+    if (!user) {
+      return res.redirect('/login'); // Redirect to login if not logged in
+    }
+  
+    res.render('pages/addOrganisation', { user });
+
+});
+
+
+  
+
+  app.post('/addOrganisation', async (req, res) => {
+    const { orgName, email, phoneNumber, logo, instaHandles, twitterHandles, linkHandles, comments } = req.body;
+
+    try {
+        // Assuming you've stored the user in the session upon login
+        const user = req.session.user;
+
+        // Check if the user is logged in
+        if (!user) {
+            return res.redirect('/login'); // Redirect to login if not logged in
+        }
+
+        console.log('User ID:', user._id);
+
+        // Access organization details from the session
+        const organization = req.session.organizationDetails || {};
+
+        // Update organization details
+        organization.orgName = orgName;
+        organization.email = email;
+        organization.phoneNumber = phoneNumber;
+        organization.logo = logo;
+        organization.instaHandles = instaHandles;
+        organization.twitterHandles = twitterHandles;
+        organization.linkHandles = linkHandles;
+        organization.comments = comments;
+
+        // Update the organization details in the session
+        req.session.user.organizationDetails = organization;
+
+        console.log('Values Updated:', organization);
+
+        // Redirect to a welcome or dashboard page
+        res.redirect('/displayOrganisation');
+
+    } catch (error) {
+        console.error('Error updating organization details:', error);
+        res.status(500).send('Internal Server Error');
+    }
+
+});
+
+
+
+
+
+
+
+
+// Add this route in your Express app
+app.get('/displayOrganisation', (req, res) => {
+    // Assuming you've stored the user in the session upon login
+    const user = req.session.user;
+    console.log(req.session); // Log the session to the console
+
+    // Check if the user is logged in
+    if (!user) {
+        return res.redirect('/login'); // Redirect to login if not logged in
+    }
+
+    // Access organization details from the session
+    const organization = req.session.organizationDetails || {};
+
+    // Render the displayOrganization page and pass both user and organization information
+    res.render('pages/displayOrganisation', { user, organization });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+
+
+
+/*Route to listen to our free port 3000 */
+app.listen(3000, () => {
+  console.log("Connected at port 3000");
+});
+
+
+
+
+
+
+
+
+
+
